648f3c3aaaafb71a7c6d80d7ca5f8700608879e6
Sebastien Rosset (serosset) <serosset@cisco.com>
2021-06-18 04:44:22 +0000

    Add unit test for percent-encoded characters

--- url_parser_test.go
+++ url_parser_test.go
@@ -2,6 +2,7 @@ package godata
 
 import (
 	"net/url"
+	"regexp"
 	"testing"
 )
 
@@ -226,4 +227,109 @@ func TestUrlParserStrictValidation(t *testing.T) {
 		return
 	}
 
+}
+
+func TestUnescapedSingleQuote(t *testing.T) {
+
+	testCases := []struct {
+		url string // The test URL
+		// Set to nil if no error is expected.
+		// If error is expected, it is used to match err.Error()
+		errRegex *regexp.Regexp
+
+		expectedTree []expectedParseNode
+	}{
+		{
+			// Unescaped single quotes.
+			// This is not a valid filter because:
+			// 1. there are two consecutive literal values, 'ab' and 'c,
+			// 2. 'c is not terminated with a quote.
+			url:      "/Books?$filter=Description eq 'ab'c'",
+			errRegex: regexp.MustCompile("No matching token for '"),
+		},
+		{
+			// Simple string with special characters.
+			url:      "/Books?$filter=Description eq 'abc'",
+			errRegex: nil,
+			expectedTree: []expectedParseNode{
+				{"eq", 0},
+				{"Description", 1},
+				{"'abc'", 1},
+			},
+		},
+		{
+			// Two consecutive single quotes.
+			// http://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_URLSyntax
+			// One of the URL syntax rules for ODATA is that single quotes within string
+			// literals are represented as two consecutive single quotes.
+			// This is done to make the input strings in the ABNF test cases more readable.
+			url:      "/Books?$filter=Description eq 'ab''c'",
+			errRegex: nil,
+			expectedTree: []expectedParseNode{
+				{"eq", 0},
+				{"Description", 1},
+				// Note below two consecutive single-quotes are the encoding of one single quote,
+				// so after the tokenization it is unescaped to one single quote.
+				{"'ab'c'", 1},
+			},
+		},
+		{
+			// http://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_URLSyntax
+			// Test single quotes escaped as %27.
+			url:      "/Books?$filter=Description eq 'O%27%27Neil'",
+			errRegex: nil,
+			expectedTree: []expectedParseNode{
+				{"eq", 0},
+				{"Description", 1},
+				// Percent-encoded character %27 must be decoded to single quote.
+				{"'O'Neil'", 1},
+			},
+		},
+		{
+			// http://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_URLSyntax
+			// Test single quotes escaped as %27.
+			// This time all single quotes are percent-encoded, including the outer single-quotes.
+			url:      "/Books?$filter=Description eq %27O%27%27Neil%27",
+			errRegex: nil,
+			expectedTree: []expectedParseNode{
+				{"eq", 0},
+				{"Description", 1},
+				// Percent-encoded character %27 must be decoded to single quote.
+				{"'O'Neil'", 1},
+			},
+		},
+	}
+	for _, testCase := range testCases {
+		parsedUrl, err := url.Parse(testCase.url)
+		if err != nil {
+			t.Errorf("Test case '%s' failed: %v", testCase.url, err)
+			continue
+		}
+		var request *GoDataRequest
+		urlQuery := parsedUrl.Query()
+		request, err = ParseRequest(parsedUrl.Path, urlQuery, false /*strict*/)
+		if testCase.errRegex == nil && err != nil {
+			t.Errorf("Test case '%s' failed: %v", testCase.url, err)
+			continue
+		} else if testCase.errRegex != nil && err == nil {
+			t.Errorf("Test case '%s' failed. Expected error but obtained nil error", testCase.url)
+			continue
+		} else if err != nil && !testCase.errRegex.MatchString(err.Error()) {
+			t.Errorf("Test case '%s' failed. Obtained error %v does not match expected regex %v",
+				testCase.url, err, testCase.errRegex)
+			continue
+		}
+		if err == nil {
+			filter := request.Query.Filter
+			if filter == nil {
+				t.Errorf("Test case '%s' failed. Parsed filter is nil", testCase.url)
+				continue
+			}
+			pos := 0
+			err = CompareTree(filter.Tree, testCase.expectedTree, &pos, 0)
+			if err != nil {
+				t.Errorf("Tree representation does not match expected value. error: %s", err.Error())
+			}
+		}
+	}
 }
